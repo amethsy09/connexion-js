9ba1ed2b4929ad8a3d33c0b58519e78583b567e9
AhmadSchema <mouhamedsy2221@gmail.com>
2025-02-07 14:34:18 +0000

    refait du design

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
   security();
   redirectrole();
+  await fetchStudents();
 });
 
 document.getElementById("form").addEventListener("submit", function (event) {
@@ -112,16 +113,48 @@ function security() {
   }
 }
 function redirectrole() {
-    const user = JSON.parse(localStorage.getItem("utilisateurConnecte"));
-    if (user) {
-      // Vérifier si l'utilisateur est déjà sur la page appropriée
-      const currentPage = window.location.pathname.split('/').pop();
-      const targetPage = `${user.role}.html`;
-      
-      if (currentPage !== targetPage) {
-        // Redirection selon le rôle de l'utilisateur
-        window.location.href = `/pages/${targetPage}`;
-      }
-    }
+  const user = JSON.parse(localStorage.getItem("utilisateurConnecte"));
+  if (user) {
+    // Vérifier si l'utilisateur est déjà sur la page appropriée
+    const currentPage = window.location.pathname.split("/").pop();
+    const targetPage = `${user.role}.html`;
+
+    // if (currentPage !== targetPage) {
+    //   // Redirection selon le rôle de l'utilisateur
+    //   console.log("Redirection vers :", targetPage);
+    //   window.location.href = `/pages/${targetPage}`;
+    // }
   }
-  
+}
+// recuperation des etudiants
+async function fetchStudents() {
+  try {
+    const response = await fetch("data.json");
+    const data = await response.json();
+    console.log(data);
+    const students = data.etudiants;
+    // console.log(students);
+    const studentsList = document.getElementById("students");
+    studentsList.innerHTML = "";
+    students.forEach((student) => {
+      const row = document.createElement("tr");
+      row.classList.add(
+        "text-center",
+        index % 2 === 0 ? "bg-gray-100" : "bg-white"
+      ); // Alternance de couleur
+
+      row.innerHTML = `
+        <td class="px-4 py-2 border">${student.nom}</td>
+        <td class="px-4 py-2 border">${student.prenom}</td>
+        <td class="px-4 py-2 border">${student.classe}</td>
+        <td class="px-4 py-2 border">${student.email}</td>
+        <td class="px-4 py-2 border">${student.telephone}</td>
+      `;
+
+      studentsList.appendChild(row);
+    });
+  } catch (error) {
+    console.error("Erreur lors de la récupération des étudiants:", error);
+  }
+}
+// Appel de la fonction pour charger les étudiants dès que la page est prête
